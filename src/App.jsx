71c2b8e0c3b0a2274c03f71d70f6e8502d550761
fabian1718 <fagodu5@gmail.com>
2024-04-22 10:05:19 -0500

    Inicia pagina oficial grupo nema

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Start from './components/Start'
+
+function App() {
+
+  return (
+    <div>
+      <Start />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Start.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Start extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Aqui se contruye el Gupo Nema</h1>
+            </div>
+        );
+    }
+}
+
+export default Start;
